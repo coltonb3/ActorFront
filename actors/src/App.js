67a7869ca399b1a69e5f037a0698f0bf3fca5500
coltonb3 <clyb333@gmail.com>
2023-01-03 08:36:57 -0600

    clean up

---
+++ actors/src/App.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import Navbar from './components/navbar';
+import Cards from './components/cards';
+import Search from './components/search';
+import Other from './components/other'
+import {Actor} from './data/data';
+
+
+function App() {
+const [query, setQuery] = useState('')
+
+const keys = ['first_name', 'DOB']
+const search = (data) => {
+   return data.filter((item) => keys.some((key) => item[key].toLowerCase().includes(query))
+   );
+}
+
+  return (
+
+    <div className="App">
+    <Navbar />
+    <div className="grid grid-cols-3 pl-2 content-center">
+    <Cards />
+    <Cards />
+    <Cards />
+
+    </div>
+    {/* seperate search function without returning multiple lines of data */}
+    {/* <Search/> */}
+
+
+{/* returning multiple lines of data/ searching all items stored locally..will have to link to a 3rd party api but should be (easy(i hope)) */}
+    <div class='flex items-center flex-col py-16'>
+       <input type="search" placeholder='search' class='w-full md:w-auto h-9 '  onChange={e=> setQuery(e.target.value)}/>
+       <br/>
+       <Other data={search(Actor)}/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ actors/src/data/data.js
@@ -0,0 +1,26 @@
+export const Actor = [
+{
+    "id": 1 ,
+    "first_name": "John",
+    "DOB": "01/01/2000"
+},
+{
+    "id": 2 ,
+    "first_name": "Colton",
+    "DOB": "07/28/1995"
+},
+
+{
+    "id": 3 ,
+    "first_name": "Lisa",
+    "DOB": "05/05/1968"
+},
+{
+    "id": 4 ,
+    "first_name": "Bob",
+    "DOB": "01/11/2003"
+}
+
+]
+
+export default Actor;
