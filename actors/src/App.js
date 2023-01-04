7594606cfa04d3f5fdc59797335ab80b5197b126
Shufeng Wu <wu666@Shufengs-MacBook-Air.local>
2023-01-04 13:54:12 -0500

    axios working

--- actors/src/App.js
+++ actors/src/App.js
@@ -5,12 +5,13 @@ import Cards from './components/cards';
 import Search from './components/search';
 import Other from './components/other'
 import Add from './components/add'
+import Edit from './components/edit'
 import {Actor} from './data/data';
 
 
 function App() {
 const [query, setQuery] = useState('')
-const [actors, setActors] = useState('')
+const [actors, setActors] = useState([])
 const keys = ['first_name', 'DOB']
 
 // search feature 
@@ -28,12 +29,22 @@ const getActors = () => {
 
 
 const handleCreate =(addActor) => {
-  axios.post('https://pacific-hollows-96763.herokuapp.com/api', addActor)
+  console.log(addActor)
+  axios.post('https://pacific-hollows-96763.herokuapp.com/api/actors', addActor)
   .then((response) =>{
     console.log(response)
     getActors()
   })
 }
+
+const handleUpdate = (editActor) => {
+  axios
+    .put('https://pacific-hollows-96763.herokuapp.com/api/actors/'+ editActor.id, editActor)
+    .then((response)=>{
+      getActors()
+    })
+}
+
 useEffect(() => {
   getActors()
  }, [])
@@ -47,16 +58,18 @@ useEffect(() => {
     <Cards />
     <Cards />
     <Add handleCreate={handleCreate}/>
-      {/* <div className='list'>
-      {actors.map((actors) => {
+    <div className='list'>
+      {actors.map((actor) => {
         return(
-          <div className='actor' key={actors.id}>
-            <h1>Name: {actors.name}</h1>
-            </div>
+          <div className='actor' key={actor.id}>
+            <h1>Name: {actor.name}</h1>
+
+            <Edit actor={actor} handleUpdate={handleUpdate}/> 
+          </div>
         )
       })}
-    </div>
-     */}
+    </div> 
+    
 
     </div>
     {/* seperate search function without returning multiple lines of data */}
--- actors/src/components/add.jsx
+++ actors/src/components/add.jsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react'
 const Add = (props) => {
     let newActor = { name: '', age: Number, knownFor: '', bio: '', imageURL: ''}
     const [actor, setActor] = useState(newActor)
+    
     const handleChange = (event) => {
         setActor({...actor, [event.target.name]: event.target.value})
       }
@@ -17,16 +18,16 @@ const Add = (props) => {
         <>
         <form className='flex flex-col' onSubmit={handleSubmit}>
             <label htmlFor="name">Name:</label>
-            <input type="text" name='name' value={actor.name} onChange={handleChange} />
+            <input type="text" name='name' onChange={handleChange} />
             <label htmlFor="age">Age: </label>
-            <input type="number" name='age' value={actor.age} onChange={handleChange} />
+            <input type="number" name='age' onChange={handleChange} />
             <input type="submit" />
             <label htmlFor="knownFor">Known For: </label>
-            <input type="text" name='knownFor' value={actor.knownFor} onChange={handleChange} />
+            <input type="text" name='knownFor' onChange={handleChange} />
             <label htmlFor="bio">Bio: </label>
-            <input type="text" name='bio' value={actor.bio} onChange={handleChange} /> 
+            <input type="text" name='bio' onChange={handleChange} /> 
             <label htmlFor="imageURL">Image: </label>
-            <input type="text" name='image' value={actor.imageURL} onChange={handleChange} />
+            <input type="text" name='imageURL' onChange={handleChange} />
         </form>
         </>
     )
